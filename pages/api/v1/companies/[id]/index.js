426a739bed10cdf60d17f2561ef44250b494a9ae
verbuscomunicacao <ferreira@verbuscomunicacao.com.br>
2026-01-26 12:19:17 -0300

    feat: add company deletion support and tests

--- pages/api/v1/companies/[id]/index.js
+++ pages/api/v1/companies/[id]/index.js
@@ -4,9 +4,9 @@ import company from "models/company"
 
 const router = createRouter()
 router.use(controller.injectAnnonymousOrUser)
-router.use(controller.canRequest("read:content"))
 
-router.patch(patchHandler)
+router.patch(controller.canRequest("update:company"), patchHandler)
+router.delete(controller.canRequest("delete:company"), deleteHandler)
 
 export default router.handler(controller.errorHandlers)
 
@@ -14,4 +14,11 @@ async function patchHandler(request, response) {
   const { id } = request.query
   const updatedCompany = await company.update(id, request.body)
   return response.status(200).json(updatedCompany)
+}
+
+async function deleteHandler(request, response) {
+  const { id } = request.query
+  await company.findOneById(id)
+  await company.deleteById(id)
+  return response.status(204).end()
 }
