d997c80b94dfc5c854658509d4113998baeed7ab
verbuscomunicacao <ferreira@verbuscomunicacao.com.br>
2026-06-02 14:54:58 -0300

    fix: changes teests

--- tests/api/integration/api/v1/rooms/post.test.js
+++ tests/api/integration/api/v1/rooms/post.test.js
@@ -161,6 +161,7 @@ describe("POST /api/v1/rooms", () => {
         hotel_name: hotel.name,
         hotel_check_in_date: hotel.check_in_date.toISOString(),
         hotel_check_out_date: hotel.check_out_date.toISOString(),
+        hotel_checkout_question: null,
         room_type: roomType.name,
         room_type_description: roomType.description,
         room_category: roomCategory.name,
